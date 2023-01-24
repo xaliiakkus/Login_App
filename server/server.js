7effb2c92aae8fdb6df9eca5d4e744806cd27da0
xaliiakkus <xaliakkus@gmail.com>
2023-01-24 22:06:49 +0300

    main

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 import cors from 'cors';
-import morgan from "morgan";
-
+import morgan from 'morgan';
+import connect from '../server/database/conn'
 const app = express();
 /** Middlewares Korsan Yazılımcılar Kullandığımız Kodların  Coğunu Göremez */
 app.use(express.json());
@@ -9,6 +9,23 @@ app.use(cors());
 app.use(morgan());
 app.disable('x-powered-by');
 
-const port = 8000;
+const port = 5781;
 
 /**HTTP GET Request */
+app.get('/', (req, res) => {
+    res.status(201).json("Home Get Request");
+});
+
+/** Api Routes */
+
+/** Start Server Only When We Have Valid Connection */
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`);
+        })
+    } catch (error) {
+        console.log('Cannot Connect to the server')
+    }
+})
+
